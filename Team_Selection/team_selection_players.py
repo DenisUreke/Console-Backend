941b58fa47e37c7787182ba7bfe1f663fbbe7f7d
DenisUreke <70695931+DenisUreke@users.noreply.github.com>
2025-07-10 23:31:02 +0200

    changes made

--- Team_Selection/team_selection_players.py
+++ Team_Selection/team_selection_players.py
@@ -1,12 +1,45 @@
+from operator import index
 from Models.player import Player
+from Team_Selection.team_selection_model import TeamSelectionModel
+import pygame
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from Orchestrator.orchestrator import Orchestrator
     
 class TeamSelectionPlayers:
-    def __init__ (self, orchestrator: 'Orchestrator'):
+    def __init__ (self, screen: pygame.Surface, model: TeamSelectionModel, orchestrator: 'Orchestrator'):
+        self.screen = screen
+        self.model = model
         self.orchestrator = orchestrator
         self.players: list[Player] = orchestrator.player_manager.players
-
-
+        self.center = 420
+        
+        '''Create a dictionary to return values so player moved depending on team'''
+        self.team = {
+            1: 100,
+            2: 420,
+            3: 740
+        }
+        
+        self.reset_team_selection()
+        
+        self.font = pygame.font.Font(None, 36)
+    
+    def render(self):
+        loc_x, loc_y = self.model.position.x, self.model.position.y
+        
+        for index, player in enumerate(self.players):
+            name_surface = self.font.render(player.name, True, (255, 255, 255))
+            
+            name_rect = name_surface.get_rect()
+            name_rect.centerx = loc_x + self.team[player.team_selection_position]
+            name_rect.top = loc_y + 100 + index * 40
+            
+            self.screen.blit(name_surface, name_rect)
+            
         '''set rules by game enum in orchestrator'''
+        
+    def reset_team_selection(self):
+        for player in self.players:
+            player.team_selection_position = 1
+            player.is_in_game = False
--- Team_Selection/team_selection_view.py
+++ Team_Selection/team_selection_view.py
@@ -2,7 +2,7 @@ import pygame
 from Enums.image_path_enum import ImagePath
 from Interfaces.view_interface import ViewInterface
 from Team_Selection.team_selection_box import TeamSelectionBox
-import os
+from Team_Selection.team_selection_players import TeamSelectionPlayers
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from Orchestrator.orchestrator import Orchestrator
@@ -14,6 +14,7 @@ class TeamSelectionView(ViewInterface):
         self.orchestrator = orchestrator
         
         self.box = TeamSelectionBox(self.screen, self.model)
+        self.players = TeamSelectionPlayers(self.screen, self.model, orchestrator)
         
         # Load background image
         image_path = ImagePath.get_image_path(self.orchestrator.state)
@@ -25,3 +26,4 @@ class TeamSelectionView(ViewInterface):
         self.screen.blit(self.background, (0, 0))
         
         self.box.render()
+        self.players.render()
