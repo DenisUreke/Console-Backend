5f64bbaa32b27a63f252b7c4c5f633013cabe386
DenisUreke <70695931+DenisUreke@users.noreply.github.com>
2025-07-10 21:28:41 +0200

    change

--- Team_Selection/team_selection_players.py
+++ Team_Selection/team_selection_players.py
@@ -1,3 +1,4 @@
+from Models.player import Player
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from Orchestrator.orchestrator import Orchestrator
@@ -5,6 +6,7 @@ if TYPE_CHECKING:
 class TeamSelectionPlayers:
     def __init__ (self, orchestrator: 'Orchestrator'):
         self.orchestrator = orchestrator
-        self.players: dict = orchestrator.player_manager.game_players_dict
-        
+        self.players: list[Player] = orchestrator.player_manager.players
+
+
         '''set rules by game enum in orchestrator'''
